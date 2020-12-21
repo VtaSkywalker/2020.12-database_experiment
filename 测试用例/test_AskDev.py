3b944d181358605a3c8796181a89f0eff8ae80b2
蔡旻轩 <1169385803@qq.com>
2020-12-21 16:47:09 +0800

    upload_by_shell

---
+++ 测试用例/test_AskDev.py
@@ -0,0 +1,6 @@
+import sys
+sys.path.append("../04-实现阶段/codes")
+
+from AskDev import *
+
+print(AskDev.askDevReq('user1', '998347', '5'))
---
+++ 测试用例/test_FixDev.py
@@ -0,0 +1,6 @@
+import sys
+sys.path.append("../04-实现阶段/codes")
+
+from FixDev import *
+
+print(FixDev.fixDevReq(["user1", 1, 0, "2020-12-21", "reason1"]))
